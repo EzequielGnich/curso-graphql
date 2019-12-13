edec094619de7960b7ae023e7b19ee61ce8c0182
EzequielGnich <ezequielgnich@live.com>
2019-12-13 10:53:54 -0300

    Criado primeiro tipo com seus atributos (Usuario)

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,9 +1,20 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+  scalar Date
   type Query {
     ola: String
-    horaAtual: String
+    horaAtual: Date
+    # horaAtual: String
+    usuarioLogado: Usuario
   }
 `;
 
@@ -13,7 +24,18 @@ const resolvers = {
       return "Hello World";
     },
     horaAtual() {
-      return `${new Date()}`;
+      return new Date();
+      // return `${new Date()}`;
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: "Ezequiel Gnich",
+        email: "ezequielgnich@live.com",
+        idade: 23,
+        salario: 2754.24,
+        vip: true
+      };
     }
   }
 };
