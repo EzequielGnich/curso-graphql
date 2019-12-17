c382ab12857584a5b84d8e8fa8b159eca0b87e97
EzequielGnich <ezequielgnich@live.com>
2019-12-17 12:24:59 -0300

    arquivo com os arrays e variaveis

---
+++ schema-query/projeto/data/db.js
@@ -0,0 +1,30 @@
+const perfis = [
+  { id: 1, nome: "comum" },
+  { id: 2, nome: "administrador" }
+];
+
+const usuarios = [
+  {
+    id: 1,
+    nome: "João Aoadee",
+    email: "joaoaoa@zemail.com",
+    idade: 29,
+    perfil_id: 1
+  },
+  {
+    id: 2,
+    nome: "Aline Acaca",
+    email: "alineacaca@hemail.com",
+    idade: 45,
+    perfil_id: 2
+  },
+  {
+    id: 3,
+    nome: " Alonsio Amanci",
+    email: "aloisioaman@cemail.com",
+    idade: 21,
+    perfil_id: 2
+  }
+];
+
+module.exports = { usuarios, perfis };
