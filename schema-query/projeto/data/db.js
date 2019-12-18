47bc8b36ae4d27b8be85729f4cb2bdc888e34fc3
EzequielGnich <ezequielgnich@live.com>
2019-12-18 12:03:05 -0300

    Adicionado campo status

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -9,21 +9,24 @@ const usuarios = [
     nome: "João Aoadee",
     email: "joaoaoa@zemail.com",
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: "Ativo"
   },
   {
     id: 2,
     nome: "Aline Acaca",
     email: "alineacaca@hemail.com",
     idade: 45,
-    perfil_id: 2
+    perfil_id: 2,
+    status: "Inativo"
   },
   {
     id: 3,
     nome: " Alonsio Amanci",
     email: "aloisioaman@cemail.com",
     idade: 21,
-    perfil_id: 2
+    perfil_id: 2,
+    status: "Bloqueado"
   }
 ];
 
