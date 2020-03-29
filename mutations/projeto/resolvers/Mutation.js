932022cde5fd1545c9f4db63012eeaf3bd4642f7
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-29 20:19:16 -0300

    Mutation para excluir usuario

--- mutations/projeto/resolvers/Mutation.js
+++ mutations/projeto/resolvers/Mutation.js
@@ -15,5 +15,16 @@ module.exports = {
 
         usuarios.push(novo)
         return novo
+    },
+
+    excluirUsuario(_, { id }) {
+        const i = usuarios.findIndex(u => u.id === id)
+
+        if (i < 0) return null
+
+        // splice(param1, param2) no param1 vai o id do que eu quero excluir
+        // e nome param2 vai a quantidade que eu gostaria de excluir
+        const excluidos = usuarios.splice(i, 1)
+        return excluidos ? excluidos[0] : null
     }
 }
