6a3aabfc9dc26673d640868c4eb0242a9f5a5bbb
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-30 20:44:51 -0300

    alterar.js

---
+++ banco-de-dados/projeto/testes/alterar.js
@@ -0,0 +1,34 @@
+const db = require("../config/db");
+
+const novoUsuario = {
+  nome: "Pedro",
+  email: "pedro@live.com",
+  senha: "123456"
+};
+
+async function exercicio() {
+  // count
+  const { qtde } = await db("usuarios")
+    .count("* as qtde")
+    .first();
+
+  if (qtde === 0) {
+    await db("usuarios").insert(novoUsuario);
+  }
+
+  //consultar
+  let { id } = await db("usuarios")
+    .select("id")
+    .limit(1)
+    .first();
+
+  await db("usuarios")
+    .where({ id })
+    .update({ nome: "Pedro" });
+
+  return db("usuarios").where({ id });
+}
+
+exercicio()
+  .then(usuario => console.log(usuario))
+  .finally(() => db.destroy());
