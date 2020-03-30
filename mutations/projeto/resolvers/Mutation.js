5b753edc63c1906b1cc874b2a0298872f7e88877
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-29 22:27:22 -0300

    Aplicação do Eslint

--- mutations/projeto/resolvers/Mutation.js
+++ mutations/projeto/resolvers/Mutation.js
@@ -1,44 +1,44 @@
-const { usuarios, proximoId }  = require('../data/db');
+const { usuarios, proximoId } = require("../data/db");
 
 module.exports = {
-    novoUsuario(_, args /*{ nome, email, idade }*/) {
-        const emailExistente = usuarios.some(u => u.email === args.email)
+  novoUsuario(_, args /*{ nome, email, idade }*/) {
+    const emailExistente = usuarios.some(u => u.email === args.email);
 
-        if(emailExistente) throw new Error('Email já cadastrado')
+    if (emailExistente) throw new Error("Email já cadastrado");
 
-        const novo = {
-            id: proximoId(),
-            ...args,
-            perfil_id: 1,
-            status: 'ATIVO'
-        }
+    const novo = {
+      id: proximoId(),
+      ...args,
+      perfil_id: 1,
+      status: "ATIVO"
+    };
 
-        usuarios.push(novo)
-        return novo
-    },
+    usuarios.push(novo);
+    return novo;
+  },
 
-    excluirUsuario(_, { id }) {
-        const i = usuarios.findIndex(u => u.id === id)
+  excluirUsuario(_, { id }) {
+    const i = usuarios.findIndex(u => u.id === id);
 
-        if (i < 0) return null
+    if (i < 0) return null;
 
-        // splice(param1, param2) no param1 vai o id do que eu quero excluir
-        // e nome param2 vai a quantidade que eu gostaria de excluir
-        const excluidos = usuarios.splice(i, 1)
-        return excluidos ? excluidos[0] : null
-    },
+    // splice(param1, param2) no param1 vai o id do que eu quero excluir
+    // e nome param2 vai a quantidade que eu gostaria de excluir
+    const excluidos = usuarios.splice(i, 1);
+    return excluidos ? excluidos[0] : null;
+  },
 
-    alterarUsuario(_, args) {
-        const i = usuarios.findIndex(u => u.id === args.id)
+  alterarUsuario(_, args) {
+    const i = usuarios.findIndex(u => u.id === args.id);
 
-        if (i < 0) return null
+    if (i < 0) return null;
 
-        const usuario = {
-            ...usuarios[i],
-            ...args
-        }
+    const usuario = {
+      ...usuarios[i],
+      ...args
+    };
 
-        usuarios.splice(i, 1, usuario)
-        return usuario
-    }
-}
+    usuarios.splice(i, 1, usuario);
+    return usuario;
+  }
+};
--- mutations/projeto/resolvers/index.js
+++ mutations/projeto/resolvers/index.js
@@ -1,9 +1,9 @@
-const Query = require('./Query')
-const Usuario = require('./Usuario')
-const Mutation = require('./Mutation')
+const Query = require("./Query");
+const Usuario = require("./Usuario");
+const Mutation = require("./Mutation");
 
 module.exports = {
-    Query,
-    Usuario,
-    Mutation
-}
+  Query,
+  Usuario,
+  Mutation
+};
