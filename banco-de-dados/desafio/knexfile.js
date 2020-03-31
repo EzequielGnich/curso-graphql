4b871f3f7835ccf68cf2c3db5bea2c6509368dea
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-31 00:14:55 -0300

    Inicio do projeto

---
+++ banco-de-dados/desafio/knexfile.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+module.exports = {
+  client: "mysql",
+  connection: {
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
