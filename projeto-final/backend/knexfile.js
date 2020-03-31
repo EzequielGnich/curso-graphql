a46f5aa6678b1a37a3198d92f461e401c1ce4bc7
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-31 20:38:36 -0300

    Inicio projeto-final

---
+++ projeto-final/backend/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  client: "mysql",
+  connection: {
+    database: "projeto-final",
+    user: "root",
+    password: "@26eg15111990eg"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
