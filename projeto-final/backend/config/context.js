e4ed6b934eee22cbdbc796ff5a41e4b909df94bf
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-01 17:10:45 -0300

    Criada a função validarUsuarioFiltro() para validar os filtro enviados pelo usuario

--- projeto-final/backend/config/context.js
+++ projeto-final/backend/config/context.js
@@ -1,9 +1,9 @@
 const jwt = require("jwt-simple");
 
 module.exports = async ({ req }) => {
-  await require("./simularUsuarioLogado")(req);
-  const auth = req.headers.authorization;
+  // await require("./simularUsuarioLogado")(req);
 
+  const auth = req.headers.authorization;
   const token = auth && auth.substring(7);
 
   let usuario = null;
@@ -19,7 +19,6 @@ module.exports = async ({ req }) => {
       console.log(e);
     }
   }
-
   if (usuario && usuario.perfis) {
     admin = usuario.perfis.includes("admin");
   }
@@ -34,6 +33,27 @@ module.exports = async ({ req }) => {
     },
     validarAdmin() {
       if (!admin) throw err;
+    },
+    validarUsuarioFiltro(filtro) {
+      // Verificar se a flag admin está true, se sim permite alterar
+      // usuario quando esta função for chamada
+      if (admin) return;
+
+      // Verifica se o usuario esta setado
+      if (!usuario) throw err;
+
+      // Verifica se o usuario enviou um filtro para poder fazer alterações
+      if (!filtro) throw err;
+
+      const { id, email } = filtro;
+      // Se não foi passado nem id nem email e lançado um excessão
+      if (!id && !email) throw err;
+      // Se o usuario passou um id, mas o id é diferente do usuario logado
+      // não é permitida a alteração
+      if (id && id !== usuario.id) throw err;
+      // Se o usuário enviou um email e o email é diferente do email do usuário
+      // logado ele lança a excessão
+      if (email && email !== usuario.email) throw err;
     }
   };
 };
