cf1a3a8d3a090614a499cae76ad05a6a9f5fa5a6
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-31 23:39:35 -0300

    Criado login da aplicação, eslint.

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -1,22 +1,37 @@
-const db = require('../../config/db')
+const db = require("../../config/db");
+const bcrypt = require("bcrypt-nodejs");
+const { getUsuarioLogado } = require("../commom/usuario");
 
 module.exports = {
-    usuarios() {
-        return db('usuarios')
-    },
-    usuario(_, { filtro }) {
-        if(!filtro) return null
-        const { id, email } = filtro
-        if(id) {
-            return db('usuarios')
-                .where({ id })
-                .first()
-        } else if(email) {
-            return db('usuarios')
-                .where({ email })
-                .first()
-        } else {
-            return null
-        }
-    },
-}
+  async login(_, { dados }) {
+    const usuario = await db("usuarios")
+      .where({ email: dados.email })
+      .first();
+
+    if (!usuario) throw new Error("Usuário/senha inválido");
+
+    const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha);
+
+    if (!saoIguais) throw new Error("Usuário/senha inválidos");
+
+    return getUsuarioLogado(usuario);
+  },
+  usuarios() {
+    return db("usuarios");
+  },
+  usuario(_, { filtro }) {
+    if (!filtro) return null;
+    const { id, email } = filtro;
+    if (id) {
+      return db("usuarios")
+        .where({ id })
+        .first();
+    } else if (email) {
+      return db("usuarios")
+        .where({ email })
+        .first();
+    } else {
+      return null;
+    }
+  }
+};
