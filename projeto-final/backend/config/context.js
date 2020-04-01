75487e816b55f99089b3a7179da1cd59fb6f039b
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-01 15:14:21 -0300

    Criada a simulação de login de usuario

--- projeto-final/backend/config/context.js
+++ projeto-final/backend/config/context.js
@@ -1,3 +1,6 @@
-module.exports = ({ req }) => {
+const jwt = require("jwt-simple");
+
+module.exports = async ({ req }) => {
+  await require("./simularUsuarioLogado")(req);
   const auth = req.headers.authorization;
 };
