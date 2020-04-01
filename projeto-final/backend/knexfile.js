80dea61f150efd6bce961a9780a61da2827ff355
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-31 21:54:16 -0300

    Configuração do arquivo .env para conexão com banco de dados

--- projeto-final/backend/knexfile.js
+++ projeto-final/backend/knexfile.js
@@ -1,11 +1,13 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
   client: "mysql",
   connection: {
-    database: "projeto-final",
-    user: "root",
-    password: "@26eg15111990eg"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
   },
   pool: {
     min: 2,
